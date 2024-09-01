b0c55dba4186bad6bc45f5305ff1ad8a44265477
camilo pinto <capir99@gmail.com>
2024-09-01 00:44:06 -0500

    prueba3

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(cors());
 app.use(
   cors({
-    origin: "nannapassfront-gi6i71x5y-nanapass-projects.vercel.app",
+    origin: "https://nannapassfront-gi6i71x5y-nanapass-projects.vercel.app",
   })
 );
 
